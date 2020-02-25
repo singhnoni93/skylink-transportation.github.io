477b4d70cbf262bede1503fb4790bf4fa363dfc3
Lakhwinder Singh <singhnoni6@gmail.com>
2020-02-24 17:11:38 -0800

    Updates

---
+++ precache-manifest.142452608e8fc065ee618d090cd3a41e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28dcf4ef8e37fd8d5bb43925fe0a929e",
+    "url": "/skylink-transportation/index.html"
+  },
+  {
+    "revision": "2d14fd39b5c158d6892c",
+    "url": "/skylink-transportation/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "45a44c1428e36b019f85",
+    "url": "/skylink-transportation/static/js/2.c933cf0a.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/skylink-transportation/static/js/2.c933cf0a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d14fd39b5c158d6892c",
+    "url": "/skylink-transportation/static/js/main.227cf6bf.chunk.js"
+  },
+  {
+    "revision": "03c6227fc7881de10282",
+    "url": "/skylink-transportation/static/js/runtime-main.ea543588.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/skylink-transportation/static/media/logo.5d5d9eef.svg"
+  }
+]);
